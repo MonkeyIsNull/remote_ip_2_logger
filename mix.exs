2654ffe0854cc2740c8544b112bb2e2d503fdb60
Adam Guyot <adam@cobenian.com>
2017-11-21 09:47:56 -0500

    updated

--- mix.exs
+++ mix.exs
@@ -7,21 +7,35 @@ defmodule RemoteIp2Logger.Mixfile do
       version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      package: package(),
+      description: description(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp description() do
+    "Puts the ip of the caller into the metadata for the Logger"
+  end
+
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:plug, "~> 1.0"}
+    ]
+  end
+
+   defp package() do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Adam Guyot"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/MonkeyIsNull/remote_ip_2_logger"}
     ]
   end
 end
