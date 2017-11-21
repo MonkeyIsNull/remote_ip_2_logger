58162ee90e339748b564d0a86b25bc1fc5fa5df3
Adam Guyot <shaolineast@gmail.com>
2017-11-21 09:44:52 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RemoteIp2Logger.Mixfile do
   def project do
     [
       app: :remote_ip_2_logger,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
